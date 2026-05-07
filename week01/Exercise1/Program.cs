e60d3a14ed852ce0da7f053a732787fd730d758d
providiah <pereforokuma@byupathway.edu>
2026-05-07 10:53:54 +0100

    added comment to the code

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -6,10 +6,14 @@ class Program
     {
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
+
+        // Display user input in Capitalized format.
         string result_1 = char.ToUpper(first[0])+ first.Substring(1);
 
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
+
+        // Display user input in Capitalized format.
         string result_2 = char.ToUpper(last[0])+ last.Substring(1);
 
         Console.WriteLine();
