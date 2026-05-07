308487458330b1c7216a3477c156c3d00463be36
providiah <pereforokuma@byupathway.edu>
2026-05-07 10:48:47 +0100

    Finished exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+        string result_1 = char.ToUpper(first[0])+ first.Substring(1);
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        string result_2 = char.ToUpper(last[0])+ last.Substring(1);
+
+        Console.WriteLine();
+
+        Console.WriteLine ($"Your name is {result_2}, {result_1} {result_2}.");
+
+
     }
 }
+ 
