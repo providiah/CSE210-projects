567e9fd51a183fa5b43d7de026cf44779a28ff62
providiah <pereforokuma@byupathway.edu>
2026-05-08 07:28:14 +0100

    wk01assignment3 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        //int guessTimes = -1;
+         //Console.Write("What is your guess? ");
+            //int guessTrial = int.Parse(Console.ReadLine());
+
+            int guess = -1;
+            int guessNo = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            guessNo++;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Guess Higher." );
+            }
+
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Guess Lower. ");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed Right.");
+            }
+            
+            
+        }    
+        Console.WriteLine($"You Made {guessNo} guesses. ");
     }
 }
